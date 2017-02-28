0951851444bb73e8eda9766b1596e9f19a4b2fe8
zhzhh <zhihongzhang_hi@163.com>
2017-02-28 23:10:39 +0800

    jpa demo

---
+++ jpa/src/main/java/com/example/service/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Created by Administrator on 2017/2/28.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name")
+    User findUser(@Param("name") String name);
+}
