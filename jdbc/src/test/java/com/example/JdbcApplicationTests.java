6e7c0dc811c596fad974edfbafdc402cb813f439
zhzhh <zhihongzhang_hi@163.com>
2017-02-28 22:15:54 +0800

    jdbcTemplate demo

--- jdbc/src/test/java/com/example/JdbcApplicationTests.java
+++ jdbc/src/test/java/com/example/JdbcApplicationTests.java
@@ -1,16 +1,41 @@
 package com.example;
 
+import com.example.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JdbcApplicationTests {
+	@Autowired
+	private UserService userSerivce;
+
+	@Before
+	public void setUp(){
+		userSerivce.deleteAllUsers();
+	}
 
 	@Test
 	public void contextLoads() {
+		// 插入5个用户
+		userSerivce.create("a", 1);
+		userSerivce.create("b", 2);
+		userSerivce.create("c", 3);
+		userSerivce.create("d", 4);
+		userSerivce.create("e", 5);
+		// 查数据库，应该有5个用户
+		Assert.assertEquals(5, userSerivce.getAllUsers().intValue());
+		// 删除两个用户
+		userSerivce.deleteByName("a");
+		userSerivce.deleteByName("e");
+		// 查数据库，应该有5个用户
+		Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
 	}
-
 }
