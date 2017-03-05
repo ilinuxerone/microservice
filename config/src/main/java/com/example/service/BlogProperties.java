81ad198ff0058c5f3bae851615ee8b4ae4b417df
zhzhh <zhihongzhang_hi@163.com>
2017-03-05 08:54:55 +0800

    springboot  config file

--- config/src/main/java/com/example/service/BlogProperties.java
+++ config/src/main/java/com/example/service/BlogProperties.java
@@ -1,7 +1,32 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
- * Created by Administrator on 2017/2/27.
+ * Created by Administrator on 2017/3/5.
  */
+@Component
 public class BlogProperties {
+    @Value("${com.example.blog.name}")
+    private String name;
+
+    @Value("${com.example.blog.title}")
+    private String title;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- config/src/test/java/com/example/ConfigApplicationTests.java
+++ config/src/test/java/com/example/ConfigApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import com.example.service.BlogProperties;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConfigApplicationTests {
 
+	@Autowired
+	private BlogProperties blogProperties;
+
 	@Test
-	public void contextLoads() {
+	public void getHello() {
+		Assert.assertEquals(blogProperties.getName(), "zzh");
+		Assert.assertEquals(blogProperties.getTitle(), "hello springboot");
 	}
 
 }
