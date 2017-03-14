5345efa773e744ce849f74d7355b7394b23c58bf
zhzhh <zhihongzhang_hi@163.com>
2017-03-14 23:31:13 +0800

    springcloud config center & client

--- springcloud-config-server/src/main/java/com/example/SpringcloudConfigCenterApplication.java
+++ springcloud-config-server/src/main/java/com/example/SpringcloudConfigCenterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+//@EnableDiscoveryClient
 public class SpringcloudConfigCenterApplication {
 
 	public static void main(String[] args) {
