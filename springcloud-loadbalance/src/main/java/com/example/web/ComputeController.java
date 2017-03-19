d79fad9e40abf827f80b635fa1fcb3f10911a612
zhzhh <zhihongzhang_hi@163.com>
2017-03-19 23:06:48 +0800

    ribbon cust strategy

--- springcloud-loadbalance/src/main/java/com/example/web/ComputeController.java
+++ springcloud-loadbalance/src/main/java/com/example/web/ComputeController.java
@@ -1,34 +1,30 @@
 package com.example.web;
 
+import com.example.configuration.RibbonConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * Created by Administrator on 2017/3/1.
+ * Created by Administrator on 2017/3/19.
  */
+
 @RestController
-public class ConsumerController {
+@RibbonClient(name = "compute-service2", configuration = RibbonConfiguration.class)//name是provider的服务
+public class ComputeController
+{
     @Autowired
-    RestTemplate restTemplate;
-
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
-    }
-
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
+    private RestTemplate restTemplate;
 
     @RequestMapping(value = "/add2", method = RequestMethod.GET)
     public String addCust() {
-        ServiceInstance serviceInstance = this.loadBalancerClient.choose("COMPUTE-SERVICE");
+/*        ServiceInstance serviceInstance = this.loadBalancerClient.choose("COMPUTE-SERVICE");
         System.out.println("===" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":"
-                + serviceInstance.getPort());// 打印当前调用服务的信息
+                + serviceInstance.getPort());// 打印当前调用服务的信息*/
         return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
     }
 }
