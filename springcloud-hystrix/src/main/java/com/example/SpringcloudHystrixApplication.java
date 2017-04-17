c2a65453a3a77043a504efbf0858e6151a2a4ca2
zhzhh <zhihongzhang_hi@163.com>
2017-04-17 23:03:33 +0800

    hystrx

--- springcloud-hystrix/src/main/java/com/example/SpringcloudHystrixApplication.java
+++ springcloud-hystrix/src/main/java/com/example/SpringcloudHystrixApplication.java
@@ -6,12 +6,16 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableTurbine
 public class SpringcloudHystrixApplication {
 	@Bean
 	@LoadBalanced
